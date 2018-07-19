643420a5387c54952c4fce76c46e2b3c1f3667af
Karla R <karla.y.reyes12@gmail.com>
2018-07-19 15:49:01 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+end
+ 
+  
