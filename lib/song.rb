a2bb624ee443c8190087a0374573aa16d9def2a6
Karla R <karla.y.reyes12@gmail.com>
2018-07-19 16:01:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,13 @@ class Song
   end
   
   def self.genre_count
-    @@genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[artist] = 1
+    end
   end
   
   def self.artist_count
